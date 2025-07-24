dfe08ad379d202371814e24c317483bb60cdaf24
chiella <chiellaeb@gmail.com>
2025-07-24 19:18:32 +0100

    11th commit

---
+++ localstack/init/init_localstack.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+echo "⏳ Bootstrapping AWS services in LocalStack..."
+
+# Create SQS queue
+awslocal --endpoint-url=http://localstack:4566 sqs create-queue --queue-name notifications-queue
+
+# Create DynamoDB Applications table
+awslocal dynamodb create-table \
+  --table-name AppTable\
+  --attribute-definitions AttributeName=Application,AttributeType=S \
+  --key-schema AttributeName=Application,KeyType=HASH \
+  --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
+
+# Create DynamoDB RequestLogs table
+awslocal dynamodb create-table \
+  --table-name RequestLogs \
+  --attribute-definitions AttributeName=Application,AttributeType=S \
+  --key-schema AttributeName=Application,KeyType=HASH \
+  --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
+
+# Insert application config into Applications table
+awslocal dynamodb put-item \
+  --table-name AppTable \
+  --item '{
+    "Application": {"S": "App1"},
+    "AppName": {"S": "CHA - Student Platform"},
+    "Email": {"S": "no-reply@cha.com"},
+    "Domain": {"S": "cha.com"},
+    "SES-Domain-ARN": {"S": "arn:aws:ses:us-east-1:000000000000:identity/cha.com"},
+    "SNS-Topic-ARN": {"S": "arn:aws:sns:us-east-1:000000000000:cha-app-push"}
+  }'
+
+echo "✅ LocalStack bootstrap complete."
+
